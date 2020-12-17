33b40e5ed392ceef403861e27cc84cdc3b247489
5FB5 <vecktor911@gmail.com>
2020-12-18 00:51:47 +0300

    added license comment, icon path string on generating stage

--- achievements_generator.py
+++ achievements_generator.py
@@ -1,7 +1,25 @@
-# 1 - создаётся главный объект с названием ачивки
-# 2 - в объекте параметры: описание, прогресс, если есть
+# File generator script code is under MIT License
+# Copyright (c) 2020 Valeriy Zubko
 
-VERSION = "1.0.0-beta"
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+VERSION = "1.0.1-beta"
 
 isStopped = False # for working of main function
 
@@ -25,7 +43,7 @@ def generateJson(data):
     with open('achievements.json', 'w') as fileResult:
         json.dump(data, fileResult)
 
-    print(colored("File created!", 'green'))
+    print(colored("File 'achievements.json' created and saved in current folder!", 'green'))
     pass
 
 while (isStopped == False):
@@ -37,7 +55,10 @@ while (isStopped == False):
 
     isHaveProgress = input("Is it have a progress? \ny/n: ")
     achievementProgressValue = input("\nEnter maximum value of a progress: ") if isHaveProgress == 'y' else 0
-    dataSet[achievementName]['progress'] = achievementProgressValue
+    dataSet[achievementName]['progress'] = int(achievementProgressValue)
+
+    achievementIconPath = input("Enter icon's path (in Godot's format): ")
+    dataSet[achievementName]['icon_path'] = achievementIconPath
 
     bContinue = input("Do you want to add new achievement?: y/n: ")
     if (bContinue == "n"):
