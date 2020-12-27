453ab253620ba8e0a8fc84f4d2c7ad158c6af7f1
5FB5 <vecktor911@gmail.com>
2020-12-27 16:58:09 +0300

    added basic functionality of achievements notification window

--- gd_achievement_system/achievements/scripts/achievement_data.gd
+++ gd_achievement_system/achievements/scripts/achievement_data.gd
@@ -3,9 +3,6 @@ extends Node
 # You can change this path
 const achievementsDataFileDefaultAddress = "res://achievements/data/achievements.json"
 
-# You must create or add in current singleton script dictionary that stores achievements data
-onready var globalAchievements = get_node("/root/Global").globalAchievements
-
 func getAchievements():
 	var file = File.new()
 	file.open(achievementsDataFileDefaultAddress, File.READ)
@@ -14,4 +11,3 @@ func getAchievements():
 	
 	return data
 	pass
-
--- gd_achievement_system/achievements/scripts/achievement_manager.gd
+++ gd_achievement_system/achievements/scripts/achievement_manager.gd
@@ -5,25 +5,36 @@ const ACHIEVEMENT_UI_NOTIFICATION_ADDRESS = "res://achievements/resources/game_u
 
 var m_achievements = {}
 
-var achievementsData = null
+var achievementsDataScript = null
 var achievementUiNotification = null
 
 func _init():
-	achievementsData = load(ACHIEVEMENT_DATA_SCRIPT_ADDRESS).new()
-	achievementUiNotification = preload(ACHIEVEMENT_UI_NOTIFICATION_ADDRESS).instance()
-	
+	achievementsDataScript = load(ACHIEVEMENT_DATA_SCRIPT_ADDRESS).new()
 	# Getting all achievement's data
-	m_achievements = achievementsData.getAchievements()
-	
-	# Test, must be in loop func
-	achievementUiNotification.get_node("achievementPanel/achievementIcon/TextureRect").texture = load(m_achievements.values()[0]['icon_path'])
-	achievementUiNotification.get_node("achievementPanel/achievementDescription/description").text = m_achievements.keys()[0]
-	achievementUiNotification.get_node("AnimationPlayer").play("popup")
-	
-	achievementUiNotification.add_to_group("achievementUi")
-	add_child(achievementUiNotification)
-
+	m_achievements = achievementsDataScript.getAchievements()
 	pass
+
+func activateAchievement(achievementIndex):
+	achievementUiNotification = preload(ACHIEVEMENT_UI_NOTIFICATION_ADDRESS).instance()
+	# Get name
+	achievementUiNotification.get_node("achievementPanel/achievementDescription/description").text = m_achievements.keys()[int(achievementIndex)]
+	# Get icon path
+	achievementUiNotification.get_node("achievementPanel/achievementIcon/TextureRect").texture = load(m_achievements.values()[int(achievementIndex)]['icon_path'])
+	add_child(achievementUiNotification)
+	achievementUiNotification.add_to_group("ui_achievement")
 	
-func _process(delta):
+	# Play animation
+	achievementUiNotification.get_node("AnimationPlayer").play("popup")
+	# Wait a few seconds
+	yield(get_tree().create_timer(5.5), "timeout")
+	# Play hide animation
+	achievementUiNotification.get_node("AnimationPlayer").play("hide")
+	# Delete node
+	yield(get_tree().create_timer(3), "timeout")
+	achievementUiNotification.queue_free()
+	pass
+
+func _ready():
+	# test
+	activateAchievement(0)
 	pass
