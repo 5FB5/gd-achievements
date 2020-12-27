c20fbaa6312f6a140776f500bc337f799edb15b7
5FB5 <vecktor911@gmail.com>
2020-12-27 19:59:13 +0300

    added checking for the presence of the achievement index in array range

--- gd_achievement_system/achievements/scripts/achievement_manager.gd
+++ gd_achievement_system/achievements/scripts/achievement_manager.gd
@@ -3,7 +3,7 @@ extends Node
 const ACHIEVEMENT_DATA_SCRIPT_ADDRESS = "res://achievements/scripts/achievement_data.gd"
 const ACHIEVEMENT_UI_NOTIFICATION_ADDRESS = "res://achievements/resources/game_ui/ui_achievements_notification.tscn"
 
-# How long achievement will be shown
+# How long achievement will be shown (seconds)
 const ACHIEVEMENT_SHOW_TIME = 4.7
 
 var m_achievements = {}
@@ -20,24 +20,28 @@ func _init():
 	pass
 
 func activateAchievement(achievementIndex):
-	achievementUiNotification = preload(ACHIEVEMENT_UI_NOTIFICATION_ADDRESS).instance()
-	add_child(achievementUiNotification)
+	if (achievementIndex <= len(m_achievements.keys()) - 1):
+		achievementUiNotification = preload(ACHIEVEMENT_UI_NOTIFICATION_ADDRESS).instance()
+		add_child(achievementUiNotification)
 	
-	# Get name
-	achievementUiNotification.get_node("achievementPanel/achievementDescription/description").text = m_achievements.keys()[int(achievementIndex)]
-	# Get icon path
-	achievementUiNotification.get_node("achievementPanel/achievementIcon/TextureRect").texture = load(m_achievements.values()[int(achievementIndex)]['icon_path'])
-	achievementUiNotification.add_to_group("ui_achievement")
+		# Set name
+		achievementUiNotification.get_node("achievementPanel/achievementDescription/description").text = m_achievements.keys()[int(achievementIndex)]
+		# Set icon path
+		achievementUiNotification.get_node("achievementPanel/achievementIcon/TextureRect").texture = load(m_achievements.values()[int(achievementIndex)]['icon_path'])
+		achievementUiNotification.add_to_group("ui_achievement")
 	
-	# Play animation
-	achievementUiNotification.get_node("AnimationPlayer").play("popup")
-	# Wait a few seconds
-	yield(get_tree().create_timer(ACHIEVEMENT_SHOW_TIME), "timeout")
-	# Play hide animation
-	achievementUiNotification.get_node("AnimationPlayer").play("hide")
-	# Delete node
-	yield(get_tree().create_timer(3), "timeout")
-	achievementUiNotification.queue_free()
+		# Play animation
+		achievementUiNotification.get_node("AudioStreamPlayer").play()
+		achievementUiNotification.get_node("AnimationPlayer").play("popup")
+		# Wait a few seconds
+		yield(get_tree().create_timer(ACHIEVEMENT_SHOW_TIME), "timeout")
+		# Play hide animation
+		achievementUiNotification.get_node("AnimationPlayer").play("hide")
+		# Delete node
+		yield(get_tree().create_timer(3), "timeout")
+		achievementUiNotification.queue_free()
+	else:
+		print("Code Error: Attempt to read an achievement under a key index that is out of range (", achievementIndex, " of ", len(m_achievements.keys()) - 1, ")")
 	
 	pass
 	
