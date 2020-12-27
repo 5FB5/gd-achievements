95e43d0a259908b6e5fe381b91dfc70676c6bd73
5FB5 <vecktor911@gmail.com>
2020-12-27 19:11:15 +0300

    added showAchievement signal for calling notification window

--- gd_achievement_system/achievements/scripts/achievement_manager.gd
+++ gd_achievement_system/achievements/scripts/achievement_manager.gd
@@ -3,10 +3,15 @@ extends Node
 const ACHIEVEMENT_DATA_SCRIPT_ADDRESS = "res://achievements/scripts/achievement_data.gd"
 const ACHIEVEMENT_UI_NOTIFICATION_ADDRESS = "res://achievements/resources/game_ui/ui_achievements_notification.tscn"
 
+# How long achievement will be shown
+const ACHIEVEMENT_SHOW_TIME = 4.7
+
 var m_achievements = {}
 
 var achievementsDataScript = null
 var achievementUiNotification = null
+
+signal showAchievement(index)
 
 func _init():
 	achievementsDataScript = load(ACHIEVEMENT_DATA_SCRIPT_ADDRESS).new()
@@ -16,25 +21,26 @@ func _init():
 
 func activateAchievement(achievementIndex):
 	achievementUiNotification = preload(ACHIEVEMENT_UI_NOTIFICATION_ADDRESS).instance()
+	add_child(achievementUiNotification)
+	
 	# Get name
 	achievementUiNotification.get_node("achievementPanel/achievementDescription/description").text = m_achievements.keys()[int(achievementIndex)]
 	# Get icon path
 	achievementUiNotification.get_node("achievementPanel/achievementIcon/TextureRect").texture = load(m_achievements.values()[int(achievementIndex)]['icon_path'])
-	add_child(achievementUiNotification)
 	achievementUiNotification.add_to_group("ui_achievement")
 	
 	# Play animation
 	achievementUiNotification.get_node("AnimationPlayer").play("popup")
 	# Wait a few seconds
-	yield(get_tree().create_timer(5.5), "timeout")
+	yield(get_tree().create_timer(ACHIEVEMENT_SHOW_TIME), "timeout")
 	# Play hide animation
 	achievementUiNotification.get_node("AnimationPlayer").play("hide")
 	# Delete node
 	yield(get_tree().create_timer(3), "timeout")
 	achievementUiNotification.queue_free()
+	
 	pass
-
-func _ready():
-	# test
-	activateAchievement(0)
+	
+func _on_root_showAchievement(index):
+	activateAchievement(index)
 	pass
