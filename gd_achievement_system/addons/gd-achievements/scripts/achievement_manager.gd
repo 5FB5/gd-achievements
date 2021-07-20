6d20a3de6ba2198dc103c4dd052403f76e17ded2
5FB5 <vecktor911@gmail.com>
2021-07-20 17:04:39 +0300

    added saving data on device, deleted audiostream node in ui scene

--- gd_achievement_system/addons/gd-achievements/scripts/achievement_manager.gd
+++ gd_achievement_system/addons/gd-achievements/scripts/achievement_manager.gd
@@ -2,42 +2,37 @@ tool
 
 extends Control
 
-const ACHIEVEMENT_DATA_SCRIPT_ADDRESS = "res://addons/gd-achievements/scripts/achievement_data.gd"
-const ACHIEVEMENT_UI_NOTIFICATION_ADDRESS = "res://addons/gd-achievements/resources/game_ui/achievements_notification.tscn"
-const ACHIEVEMENT_SHOW_END_TIME = 1.5
+const ACHIEVEMENT_DATA_SCRIPT_PATH = "res://addons/gd-achievements/scripts/achievement_data.gd"
+const ACHIEVEMENT_UI_NOTIFICATION_PATH = "res://addons/gd-achievements/resources/game_ui/achievements_notification.tscn"
+const ACHIEVEMENT_JSON_USER_PATH = "user://achievements.json"
+const ACHIEVEMENT_JSON_REFERENCE_PATH = "res://addons/gd-achievements/resources/data/achievemenets.json"
 
 # How long achievement will be shown (seconds)
 export var SHOW_TIME = 4.7
+const SHOW_END_TIME = 1.5
+
 # Global sound for all achievements
 export var globalSound = preload("res://addons/gd-achievements/resources/sounds/achievement_earned.wav")
-# -200 = mute, -20 = default volume for notification ('cause basic sound is too loud), 0 = original sound's volume
+
+# -200 = mute, 
+# -20 = default volume for notification ('cause basic sound is too loud), 
+# 0 = original sound's volume
 export var globalSoundVolume = -20.0
 
-# Main array with achievements
-var m_achievements = {}
-
+var m_achievements = {} # Main array with achievements
 var achievementCount = 0
 var achievementCurrentPosY = 0
-
 var achievementsDataScript = null
 var achievementUiNotificationInstance = null
-
 var soundNode = AudioStreamPlayer.new()
 
 # Main call signal that accepts index of an achievement from array
 signal showAchievement(index)
 
 func _init():
-	achievementsDataScript = load(ACHIEVEMENT_DATA_SCRIPT_ADDRESS).new()
-	# Getting all achievement's data
+	achievementsDataScript = load(ACHIEVEMENT_DATA_SCRIPT_PATH).new()
+	# Get all achievement's data
 	m_achievements = achievementsDataScript.getAchievements()
-	pass
-
-func _ready():
-	# Spawn AudioStreamPlayer node
-	initSoundNode()
-	
-	connect("showAchievement", self, "activateAchievement")
 	pass
 
 # Create audio node for playing sound
@@ -46,17 +41,69 @@ func initSoundNode():
 	soundNode.set_volume_db(globalSoundVolume)
 	add_child(soundNode)
 	pass
+
+func updateReferenceJson(achievementsFromUserFileBuf):
+	if (m_achievements.hash() != achievementsFromUserFileBuf.hash()):
+			print("AchievementSystem: User file and reference file are different. Updating data...")
+			m_achievements = achievementsFromUserFileBuf
+			print("AchievementSystem: Data updated!")
+			pass
+	pass
+
+func updateUserJson():
+	var userFile = File.new()
+	userFile.open(ACHIEVEMENT_JSON_USER_PATH, File.WRITE)
+	userFile.store_string(to_json(m_achievements))
+	userFile.close()
+	pass
+
+func checkFileJsonOnDevice():
+	var jsonUserFile = File.new()
+	
+	if (not jsonUserFile.file_exists(ACHIEVEMENT_JSON_USER_PATH)):
+		print("AchievementSystem: 'achievements.json' not found on device. Creating...")
+		jsonUserFile.open(ACHIEVEMENT_JSON_USER_PATH, File.WRITE)
+		jsonUserFile.store_line(to_json(m_achievements))
+		jsonUserFile.close()
+		print("AchievementSystem: File created!")
+		pass
+	else:
+		jsonUserFile.open(ACHIEVEMENT_JSON_USER_PATH, File.READ)
+		var achievementsFromUserFileBuf = parse_json(jsonUserFile.get_as_text())
+		jsonUserFile.close()
+		
+		updateReferenceJson(achievementsFromUserFileBuf)
+		pass
+	pass
+
+func _notification(what):
+	if (what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST):
+		updateUserJson()
+		pass
+	pass
+
+func _ready():
+	initSoundNode()
+	checkFileJsonOnDevice()
+	# Bind main signal to function
+	connect("showAchievement", self, "activateAchievement")
+	pass
 	
 func activateAchievement(achievementIndex):
-	if ((achievementCount >= 0) and (achievementCount <= len(m_achievements.keys()) - 1) and (achievementIndex <= len(m_achievements.keys()) - 1)):
+	if (achievementIndex > len(m_achievements.keys()) - 1):
+		print("AchievementSystem Error: Attempt to get an achievement on " + achievementIndex + 
+			" index that is out of range (" + (len(m_achievements.keys()) - 1) + ")")
+		return
+
+	if ((achievementCount >= 0) and (achievementCount <= len(m_achievements.keys()) - 1) and (m_achievements.values()[achievementIndex]["is_have"] == 0)):
+		m_achievements.values()[achievementIndex]["is_have"] = 1
 		# Preload instance of scene
-		achievementUiNotificationInstance = preload(ACHIEVEMENT_UI_NOTIFICATION_ADDRESS).instance()
+		achievementUiNotificationInstance = preload(ACHIEVEMENT_UI_NOTIFICATION_PATH).instance()
 		# Get ID for a new instance
 		var achievementUiNotificationId = achievementUiNotificationInstance.get_instance_id()
 		# Create instance with unique ID for independent working with its own nodes
 		var achievementUiNotification = instance_from_id(achievementUiNotificationId)
 		add_child(achievementUiNotification)
-		achievementUiNotification.add_to_group("ui_achievement")
 		
 		# Increase count of UI instances for correct position
 		achievementCount += 1
@@ -69,10 +116,15 @@ func activateAchievement(achievementIndex):
 		
 		# Set name
 		achievementUiNotification.get_node("achievementPanel/achievementDescription/description").text = m_achievements.keys()[int(achievementIndex)]
+		
 		# Set icon path
-		achievementUiNotification.get_node("achievementPanel/achievementIcon/TextureRect").texture = load(m_achievements.values()[int(achievementIndex)]['icon_path'])
+		achievementUiNotification.get_node("achievementPanel/achievementIcon/TextureRect").texture = load(
+			m_achievements.values()[int(achievementIndex)]['icon_path']
+			)
+		
 		# Play sound
 		soundNode.play()
+		
 		# Play animation
 		achievementUiNotification.get_node("AnimationPlayer").play("popup")
 		
@@ -84,11 +136,6 @@ func activateAchievement(achievementIndex):
 		achievementUiNotification.get_node("AnimationPlayer").play("hide")
 		
 		# Delete node
-		yield(get_tree().create_timer(ACHIEVEMENT_SHOW_END_TIME), "timeout")
-		achievementUiNotification.queue_free()
-
-	else:
-		print("Error: Attempt to read an achievement from array under a key index that is out of range (", 
-				achievementIndex, " of ", len(m_achievements.keys()) - 1, ")")
-	
+		yield(get_tree().create_timer(SHOW_END_TIME), "timeout")
+		achievementUiNotification.queue_free()	
 	pass
